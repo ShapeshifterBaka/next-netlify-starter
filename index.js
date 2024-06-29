bdd76f3c825ccb27ded5e92b5b45227857ffff40
ShapeshifterBaka <shapeshifter46290@gmail.com>
2024-06-29 23:46:26 +0530

    index.js

---
+++ index.js
@@ -0,0 +1,62 @@
+<html lang="en" dir="ltr"><head>
+   <meta charset="utf-8">
+   <title>Alter-Discord Bot</title>                                                         
+  
+   <link rel="stylesheet" href="index.css">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+   <link rel="icon" href="">
+   <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,600,700,800,900&amp;display=swap" rel="stylesheet">
+
+   <link href="https://fonts.googleapis.com/css?family=Montserrat:400,600,700,800,900&amp;display=swap" rel="stylesheet">
+
+   <link href="https://fonts.googleapis.com/css?family=Lato:300,400,600,700,800,900&amp;display=swap" rel="stylesheet">
+</head>
+
+<body onload="loaderFunction()">
+
+   <div id="loader" style="display: none;">
+      <div class="loading">
+         <span></span>
+      </div>
+   </div>
+   <div id="page" style="display: block;">
+      <nav>
+         <div class="logo noselect">
+            <img class="noselect" src=""> Eternity 
+         </div>
+
+         <div class="openMenu noselect"><i class="fa fa-bars"></i></div>
+         <ul class="mainMenu noselect">
+            <li><a class="active" href="#"><i class="fas fa-home"></i> Home</a></li>
+            <li><a href="https://discord.gg/pdZHK3R48V" target="_none"><i class="fas fa-headset"></i> Support Server</a></li>
+            <div class="closeMenu noselect"><i class="fa fa-times"></i></div>
+         </ul>
+      </nav>
+
+      <div class="content noselect">
+         <div class="heading text-center hover-lazy-felix">
+            <img id="botLogo" class="noselect" src="https://images-ext-1.discordapp.net/external/O7Gm28yi_FQ0EmIhG9A7tTYRelLjd71sgIkd70gw3JA/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/1222972620006227978/8ce051bc1f897b9c17c116d7ae8fedea.png?format=webp&amp;quality=lossless&amp;width=559&amp;height=559" alt="botLogo">                                        
+  
+  
+            <h1 class="title"Alter</h1>
+            <p class="subtitle"></p>
+
+            <br>
+            <a class="btn btn-secondary" href="https://discord.gg/RN2hrY9K98" target="_none" role="button"><i class="fab fa-discord"></i>Support Server</a>                                                                             
+  
+  
+            <a class="btn btn-secondary" href="https://dsc.gg/alterinv" target="_none" role="button"><i class="🔗"></i> Invite Alter </a>                                                                                                                        
+  
+  
+  
+
+            <div>
+            </div>
+            <script src="index.js"></script>
+         <button class="lazy-felix lazy-felix-download-btn" data-img-link="https://images-ext-1.discordapp.net/external/O7Gm28yi_FQ0EmIhG9A7tTYRelLjd71sgIkd70gw3JA/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/1222972620006227978/8ce051bc1f897b9c17c116d7ae8fedea.png?format=webp&amp;quality=lossless&amp;width=559&amp;height=559"><svg xmlns="http://www.w3.org/2000/svg" class="lazyfelix-icon" width="27" height="25" viewBox="0 0 27 25" fill="none">    <path d="M13.4518 13.0377C11.4186 12.7365 6.4864 16.878 5.39453 21.7726V22.0362C5.48795 24.1899 7.75902 24.9194 10.9292 24.0316C13.0111 23.1639 14.1728 23.1375 16.125 24.0316C17.9698 25.1612 20.41 23.7051 20.643 22.0362C20.3191 18.4517 16.3154 13.1956 13.4518 13.0377Z" fill="#7A7A7A" fill-opacity="0.7"></path>    <ellipse cx="4.85476" cy="11.946" rx="2.97265" ry="4.24369" transform="rotate(-21.5283 4.85476 11.946)" fill="#7A7A7A" fill-opacity="0.7"></ellipse>    <ellipse cx="22.0599" cy="13.5489" rx="2.97265" ry="4.24369" transform="rotate(22.9527 22.0599 13.5489)" fill="#7A7A7A" fill-opacity="0.7"></ellipse>    <ellipse cx="10.1354" cy="5.66514" rx="2.92739" ry="4.7215" transform="rotate(-9.76985 10.1354 5.66514)" fill="#7A7A7A" fill-opacity="0.7"></ellipse>    <ellipse cx="17.552" cy="5.95842" rx="2.92739" ry="4.7215" transform="rotate(14.6303 17.552 5.95842)" fill="#7A7A7A" fill-opacity="0.7"></ellipse>
+  </svg></button></div>
+
+
+</div></div><div id="snackbarSuccess">Successfully added.</div><div id="snackbarDrag">please Drag and drop this Image.</div><div id="snackbarSuccess1">Successfully Removed.</div><div id="snackbarError" style="z-index: 1000">You have reached the daily limit of 5, please purchase the individual or corporate package.</div></body></html>
